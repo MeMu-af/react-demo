54db27939e6cdfb2de6517323aee7d32e0458592
Trevor <trevlewmath@gmail.com>
2025-03-15 12:37:00 -0400

    Update 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
 import { useState } from 'react';
-import {puppyList} from './data.js';
+import { puppyList } from './data.js';
 import './App.css';
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
   console.log("puppyList: ", puppyList);
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              console.log("puppy id: ", puppy.id);
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        (() => {
+          const featuredPup = puppies.find((pup) => pup.id === featPupId);
+          console.log("featuredPup: ", featuredPup);
+          return (
+            <div>
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div>
+          );
+        })()
+      )}
     </div>
   );
 }
